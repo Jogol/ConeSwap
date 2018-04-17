cfdae343b33ec6a3fc8cb9a6444e367e90b50de6
Jonathan Golan <Jonathanbgolan@gmail.com>
2018-04-17 13:41:57 +0200

    Added display

--- Assets/Scripts/BinaryManager.cs
+++ Assets/Scripts/BinaryManager.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BinaryManager : MonoBehaviour {
 
-    public TextMesh text;
+    public TextMesh current;
+    public TextMesh target;
+    public TextMesh result;
     public GameObject box1;
     public GameObject box2;
     public GameObject box3;
@@ -16,6 +19,9 @@ public class BinaryManager : MonoBehaviour {
     ClosestNum num3;
     ClosestNum num4;
     ClosestNum num5;
+
+    int targetNum;
+    int displayFrames = 0;
     // Use this for initialization
     void Start () {
         num1 = box1.GetComponent<ClosestNum>();
@@ -23,11 +29,35 @@ public class BinaryManager : MonoBehaviour {
         num3 = box3.GetComponent<ClosestNum>();
         num4 = box4.GetComponent<ClosestNum>();
         num5 = box5.GetComponent<ClosestNum>();
+
+        targetNum = GetRand();
+        target.text = targetNum.ToString();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        string numbers = "" + num1.GetNum() + " " + num2.GetNum() + " " + num3.GetNum() + " " + num4.GetNum() + " " + num5.GetNum();
-        text.text = numbers;
+        if (displayFrames == 0)
+        {
+            result.text = "";
+        }
+
+        string numbers = "" + num1.GetNum() + num2.GetNum() + num3.GetNum() + num4.GetNum() + num5.GetNum();
+        int b = Convert.ToInt32(numbers, 2);
+        current.text = b.ToString();
+        if (targetNum == b)
+        {
+            result.text = "Correct!";
+            displayFrames = 200;
+            targetNum = GetRand();
+            target.text = targetNum.ToString();
+        } else
+        {
+            displayFrames--;
+        }
 	}
+
+    int GetRand()
+    {
+        return UnityEngine.Random.Range(0, 32);
+    }
 }
